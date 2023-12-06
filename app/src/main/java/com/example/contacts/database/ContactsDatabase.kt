956a504e6f82ccce6a68f63acc13ff7e697b61d4
MohammadAshrf <mohamedashrf155@gmail.com>
2023-12-06 16:54:46 +0200

    Initial setup

--- app/src/main/java/com/example/contacts/database/ContactsDatabase.kt
+++ app/src/main/java/com/example/contacts/database/ContactsDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Contacts::class], version = 4)
+@Database(entities = [Contacts::class], version = 5)
 abstract class ContactsDatabase : RoomDatabase() {
     abstract fun getContactsDao(): ContactsDao
 
